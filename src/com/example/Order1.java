7f762da9941f2a7075d74aea6caf45b182935e3b
upanya <upanya.pai@people10.com>
2021-10-18 12:06:20 +0530

    if then else

---
+++ src/com/example/Order1.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Order1 {
+
+    public static void main(String[] args) {
+
+        boolean isFilled = false;
+
+        // Write an if-then-else statement:
+        if (isFilled) {
+
+            System.out.println("Shipping");
+
+        }
+        else {
+
+            System.out.println("Order not ready");
+
+        }
+
+    }
+
+}
