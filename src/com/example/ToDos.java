f786758f3e11edab0d21133fa14f15fb8f0ee1e3
upanya <upanya.pai@people10.com>
2021-10-19 17:04:28 +0530

    array list

---
+++ src/com/example/ToDos.java
@@ -0,0 +1,15 @@
+package com.example;
+
+// import the ArrayList package here:
+import java.util.ArrayList;
+
+class ToDos {
+
+    public static void main(String[] args) {
+
+        // Create toDoList below:
+        ArrayList<String> toDoList = new ArrayList<String>();
+
+    }
+
+}
