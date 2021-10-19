97714f85f3e2eccb3feaae2971f5d894f4385d3e
upanya <upanya.pai@people10.com>
2021-10-19 14:58:08 +0530

    Importing Arrays

---
+++ src/com/example/Newsfeed1.java
@@ -0,0 +1,23 @@
+package com.example;
+
+// import the Arrays package here:
+import java.util.Arrays;
+public class Newsfeed1 {
+
+
+    public Newsfeed1(){
+
+    }
+
+    public String[] getTopics(){
+        String[] topics = {"Opinion", "Tech", "Science", "Health"};
+        return topics;
+    }
+
+
+    public static void main(String[] args){
+        Newsfeed1 sampleFeed = new Newsfeed1();
+        String[] topics = sampleFeed.getTopics();
+        System.out.println(Arrays.toString(topics));
+    }
+}
