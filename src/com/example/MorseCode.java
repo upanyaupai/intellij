a6dd560fe27633c335decead9cafe337dda739e8
upanya <upanya.pai@people10.com>
2021-10-21 16:47:46 +0530

    string index of

---
+++ src/com/example/MorseCode.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class MorseCode {
+
+    public static void main(String[] args) {
+
+        String code = ".... .. / .. .----. -- / ... --- -. -. -.-- / .- -. -.. / .. / .-.. .. -.- . / - --- / . .- - / .-. .- -- . -.";
+
+        // Write the code:
+        System.out.println(code.indexOf(". .- -"));
+
+    }
+
+}
