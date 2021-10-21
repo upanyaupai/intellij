b67975ae8fe9a6ef6c22fa26c31044389112b3bf
upanya <upanya.pai@people10.com>
2021-10-21 16:54:24 +0530

    string charAt

---
+++ src/com/example/Initials.java
@@ -0,0 +1,16 @@
+package com.example;
+public class Initials {
+
+    public static void main(String[] args) {
+
+        // Add a first name and a last name:
+        String firstName = "Sonny";
+        String lastName = "Li";
+
+        // What are the initials?
+        System.out.println(firstName.charAt(0));
+        System.out.println(lastName.charAt(0));
+
+    }
+
+}
