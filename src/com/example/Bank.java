4bf743e36d2fd8a2179f95d45a05b1eff82fdad8
upanya <upanya.pai@people10.com>
2021-10-25 11:39:44 +0530

    examples-

--- src/com/example/Bank.java
+++ src/com/example/Bank.java
@@ -5,13 +5,13 @@ public class Bank{
     private CheckingAccount accountTwo;
 
     public Bank(){
-        accountOne = new CheckingAccount("Zeus", 100, "1");
-        accountTwo = new CheckingAccount("Hades", 200, "2");
+        accountOne = new CheckingAccount("Zeus", 100);
+        accountTwo = new CheckingAccount("Hades", 200);
     }
 
     public static void main(String[] args){
         Bank bankOfGods = new Bank();
-        System.out.println(bankOfGods.accountOne.name);
+        System.out.println(bankOfGods.accountOne.getName());
         bankOfGods.accountOne.addFunds(5);
         bankOfGods.accountOne.getInfo();
 
